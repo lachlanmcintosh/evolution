570c211bc0b041e0f51b3115f877df225c193a32
lachlanmcintosh <lachlanmcintosh.72@gmail.com>
2023-07-18 20:10:04 +1000

    Added files

--- add_all.py
+++ add_all.py
@@ -6,27 +6,21 @@ def git_add_files():
     cwd = os.getcwd()
 
     # Get a list of all directories in the current directory
-    directories = [dir for dir in os.listdir(cwd) 
-                   if os.path.isdir(os.path.join(cwd, dir)) and 
-                   not os.path.islink(os.path.join(cwd, dir)) and 
+    directories = [dir for dir in os.listdir(cwd)
+                   if os.path.isdir(os.path.join(cwd, dir)) and
+                   not os.path.islink(os.path.join(cwd, dir)) and
                    dir not in ['temp_nix_store', 'environments']]
 
     # Add directories to the list
     directories.append(cwd)  # add the current directory to the list
 
     for directory in directories:
-        # Change directory to the given directory
-        os.chdir(directory)
-
         # Get a list of all files in the current directory
-        files_in_dir = [f for f in os.listdir('.') if os.path.isfile(f)]
+        files_in_dir = [os.path.join(cwd, directory, f) for f in os.listdir(os.path.join(cwd, directory)) if os.path.isfile(os.path.join(cwd, directory, f))]
 
         # Add each file to the Git repository
         for file_name in files_in_dir:
             subprocess.run(["git", "add", file_name])
-
-        # Change directory back to the original directory
-        os.chdir(cwd)
 
     # Commit the changes
     subprocess.run(["git", "commit", "-m", "Added files"])
